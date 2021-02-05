6244eb7e2af634123daa6aa37be78553b4ca7e50
nerizza98 <nmflores98@yahoo.com>
2021-02-05 22:20:10 +0000

    Added and modified features 3

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,9 +3,9 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// This class offers methods for converting a given 
-    /// distance measured in miles to the equicalent 
-    /// distance measured in feet
+    /// Rhis App will prompt the user to input a distance
+    /// measured in one unit and it will calculate and 
+    /// output the equicalent distance in another unit.
     /// </summary>
     /// <author>
     /// Nerizza Flores version 0.1
@@ -14,35 +14,107 @@ namespace ConsoleAppProject.App01
     {
         public const int FEET_IN_MILES = 5280;
         public const double METRES_IN_MILES = 1609.34;
-        private double miles;
-        private double feet;
-        private double metres;
-        
+        public const double FEET_IN_METRES = 3.28084;
+
+        public const string FEET = "Feet";
+        public const string MILES = "Miles";
+        public const string METRES = "Metres";
+
+        private double fromDistance;
+        private double toDistance;
+
+        private string fromUnit;
+        private string toUnit;
+
+        public DistanceConverter()
+        {
+            fromUnit = MILES;
+            toUnit = FEET;
+        }
+
         /// <summary>
         /// Call other methods
         /// </summary>
-        public void MilesToFeet()
+        public void ConvertDistance()
         {
-            OutputHeading("Converting Miles to Feet");
-            miles = InputDistance("Please enter the number of miles > ");
-            CalculateFeet();
-            OutputDistance(miles, "miles", feet, "feet");
+            OutputHeading();
+
+            fromUnit = SelectUnit("Please select the from distance unit >");
+            toUnit = SelectUnit("Please select the to distance unit > ");
+
+            Console.WriteLine($"Converting {fromUnit} to {toUnit}\n");
+
+            fromDistance = InputDistance($"Please enter the number of {fromUnit} > ");
+            CalculateDistance();
+            OutputDistance();
         }
 
-        public void FeetToMiles()
+        private void CalculateDistance()
         {
-            OutputHeading("Converting Feet to Miles");
-            feet = InputDistance("Please enter the number of feet > ");
-            CalculateMiles();
-            OutputDistance(feet, "feet", miles, "miles");
+            if (fromUnit == MILES && toUnit == FEET)
+            {
+                toDistance = fromDistance * FEET_IN_MILES;
+            }
+            else if (fromUnit == FEET && toUnit == MILES)
+            {
+                toDistance = fromDistance / FEET_IN_MILES;
+            }
+            else if (fromUnit == MILES && toUnit == METRES)
+            {
+                toDistance = fromDistance * METRES_IN_MILES;
+            }
+            else if (fromUnit == METRES && toUnit == MILES)
+            {
+                toDistance = fromDistance / METRES_IN_MILES;
+            }
+            else if (fromUnit == FEET && toUnit == METRES)
+            {
+                toDistance = fromDistance * FEET_IN_METRES;
+            }
+            else if (fromUnit == METRES && toUnit == FEET)
+            {
+                toDistance = fromDistance / FEET_IN_METRES;
+            }
         }
 
-        public void MilesToMetres()
+        private string SelectUnit(string prompt)
         {
-            OutputHeading("Converting to Miles to Metres");
-            miles = InputDistance("Please enter the number of miles > ");
-            CalculateMetres();
-            OutputDistance(miles, "miles", metres, "metres");
+            string choice = DisplayChoice(prompt);
+
+            string unit = ExecuteChoice(choice);
+            Console.WriteLine($"\nYou have chosen {unit}");
+            return unit;
+        }
+
+        private static string ExecuteChoice(string choice)
+        {
+            if (choice.Equals("1"))
+            {
+                return FEET;
+            }
+            else if (choice.Equals("2"))
+            {
+                return METRES;
+            }
+            else if (choice.Equals("3"))
+            {
+                return MILES;
+            }
+
+            return null;
+        }
+
+        private static string DisplayChoice(string prompt)
+        {
+            Console.WriteLine();
+            Console.WriteLine($" 1. {FEET}");
+            Console.WriteLine($" 2. {METRES}");
+            Console.WriteLine($" 3. {MILES}");
+            Console.WriteLine();
+
+            Console.Write(prompt);
+            string choice = Console.ReadLine();
+            return choice;
         }
 
         private double InputDistance(string prompt)
@@ -52,50 +124,21 @@ namespace ConsoleAppProject.App01
             return Convert.ToDouble(value);
         }
 
-        private void CalculateMiles()
+        private void OutputDistance()
         {
-            miles = feet / FEET_IN_MILES ;
-        
+            Console.WriteLine($"\n{fromDistance}  {fromUnit} is {toDistance} {toUnit}.\n");
         }
-
-        /// <summary>
-        /// Calculate the feet
-        /// Convert miles to feet
-        /// </summary>
-        private void CalculateFeet()
-        {
-            feet = miles * FEET_IN_MILES;
-        }
-
-
-        private void CalculateMetres()
-        {
-            metres = miles * METRES_IN_MILES;
-        }
-
-        private void OutputDistance(double fromDistance, string fromUnit, double toDistance, string toUnit)
-        {
-            Console.WriteLine($" {fromDistance}  {fromUnit} is {toDistance} {toUnit}.");
-        }
-
-
 
         /// <summary>
         /// Print out the heading
         /// </summary>
-        private void OutputHeading(string prompt)
+        private void OutputHeading()
         {
             Console.WriteLine();
             Console.WriteLine("\n------------------------------");
             Console.WriteLine("\tDistance Converter     ");
             Console.WriteLine("\tby Nerizza Flores       ");
             Console.WriteLine("------------------------------\n");
-
-            Console.WriteLine(prompt);
-            Console.WriteLine();
         }
-
-
-
     }
 }
