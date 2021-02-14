ddc83d03d6ebb9ce1145fabf3c5700e4e2f17096
nerizza98 <nmflores98@yahoo.com>
2021-02-14 12:32:48 +0000

    Added Console Helper

---
+++ ConsoleAppProject/ConsoleHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject
+{
+    public static class ConsoleHelper
+    {
+        public static void OutputHeading(string title)
+        {
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"\t{title}    ");
+            Console.WriteLine("\tby Nerizza Flores       ");
+            Console.WriteLine("------------------------------\n");
+        }
+    }
+}
